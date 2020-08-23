e6964bfb432d89ff6a78619a860c912c40689597
Keith-Njagi <kitgiana.keith@gmail.com>
2020-08-23 12:40:44 +0300

    Salesman cleaning

--- app/resources/salesmen.py
+++ app/resources/salesmen.py
@@ -28,7 +28,7 @@ salesmen_schema = SalesmanSchema(many=True)
 @api.route('')
 class SalesmanList(Resource):
     @jwt_required
-    @api.doc('fetch_salesmen')
+    @api.doc('Fetch salesmen')
     def get(self):
         '''Fetch Salesmen'''
         claims = get_jwt_claims()
@@ -52,7 +52,7 @@ class SalesmanList(Resource):
             print('========================================')
             print('Error description: ', e)
             print('========================================')
-            return {'message': 'Couldn\'t fetch salesmen'}, 400
+            return {'message': 'Could not fetch salesmen'}, 500
 
     @jwt_required
     @api.doc('register_salesman')
@@ -62,26 +62,26 @@ class SalesmanList(Resource):
         claims = get_jwt_claims()
         if not claims['is_admin']:
             return {'message': 'You are not allowed to access this resource'}, 403
-        
-        data = api.payload
-        if not data:
-            return {'message': 'No input data detected'}, 400
-
-        user_id = data['user_id']
-
-        authorization = request.headers.get('Authorization')
-        auth_token  = { "Authorization": authorization}
-
-        url = 'http://172.18.0.1:3100/api/user/' + str(user_id) # 172.18.0.3
-        req = requests.get(url, headers=auth_token)
-        if req.status_code != 200:    
-            if '\"message\"' in req.text: # {\n    \"message\": \"User does not exist\"\n}\n
-                msg = req.text.split('\"')
-                return {'message':msg[3]},req.status_code
-            return {req.text}, req.status_code
-
-        limit = data['limit']
         try:
+            data = api.payload
+            if not data:
+                return {'message': 'No input data detected'}, 400
+
+            user_id = data['user_id']
+
+            authorization = request.headers.get('Authorization')
+            auth_token  = { "Authorization": authorization}
+
+            url = 'http://172.18.0.1:3100/api/user/' + str(user_id) # 172.18.0.3
+            req = requests.get(url, headers=auth_token)
+            if req.status_code != 200:    
+                if '\"message\"' in req.text: # {\n    \"message\": \"User does not exist\"\n}\n
+                    msg = req.text.split('\"')
+                    return {'message':msg[3]},req.status_code
+                return {req.text}, req.status_code
+
+            limit = data['limit']
+        
             db_salesman = Salesman.fetch_by_user_id(user_id)
             my_salesman = salesman_schema.dump(db_salesman)
             if len(my_salesman) != 0:
@@ -100,7 +100,7 @@ class SalesmanList(Resource):
             print('========================================')
             print('Error description: ', e)
             print('========================================')
-            return {'message': 'Couldn\'t add new salesman'}, 400
+            return {'message': 'Could not add salesman'}, 500
 
 # - '/salesman/<int:id>'
 # get one salesman - Admin, Sales Man(user_id) 
@@ -110,47 +110,53 @@ class SalesmanList(Resource):
 @api.param('id', 'The salesman identifier')
 class SalesmanOperations(Resource):
     @jwt_required
-    @api.doc('get_salesman')
+    @api.doc('Get one salesman')
     def get(self, id):
         '''Get one Salesman'''
-        authorised_user = get_jwt_identity()
-        claims = get_jwt_claims()
+        try:
+            authorised_user = get_jwt_identity()
+            claims = get_jwt_claims()
 
-        db_salesman = Salesman.fetch_by_id(id)
-        salesman = salesman_schema.dump(db_salesman)
-        if len(salesman) != 0:
-            if authorised_user['id'] == salesman['user_id'] or claims['is_admin']:
-                
+            db_salesman = Salesman.fetch_by_id(id)
+            salesman = salesman_schema.dump(db_salesman)
+            if len(salesman) != 0:
+                if authorised_user['id'] == salesman['user_id'] or claims['is_admin']:
+                    
 
-                # Record this event in user's logs
-                log_method = 'get'
-                log_description = 'Fetched salesman <' + str(id) + '>' 
-                authorization = request.headers.get('Authorization')
-                auth_token  = { "Authorization": authorization}
-                record_user_log(auth_token, log_method, log_description)
+                    # Record this event in user's logs
+                    log_method = 'get'
+                    log_description = 'Fetched salesman <' + str(id) + '>' 
+                    authorization = request.headers.get('Authorization')
+                    auth_token  = { "Authorization": authorization}
+                    record_user_log(auth_token, log_method, log_description)
 
-                return {'salesman': salesman}, 200
-            return {'message':'You are not authorised to view this salesman!'}, 403
-        return {'message': 'There is no such record!'}, 404
+                    return {'salesman': salesman}, 200
+                return {'message':'You are not authorised to view this salesman!'}, 403
+            return {'message': 'There is no such record!'}, 404
+        except Exception as e:
+            print('========================================')
+            print('Error description: ', e)
+            print('========================================')
+            return{'message':'Could not fetch salesman.'}, 500
         
     @jwt_required
-    @api.doc('edit_credit_limits')
+    @api.doc('Edit credit limits')
     @api.expect(edit_salesman_model)
     def put(self, id):
         '''Edit Credit limits'''
         claims = get_jwt_claims()
         if not claims['is_admin']:
             return {'message': 'You are not authorised to edit this record!'}, 403
-        
-        db_salesman = Salesman.fetch_by_id(id)
-        this_salesman = salesman_schema.dump(db_salesman)
-        if len(this_salesman) == 0:
-            return {'message': 'This salesman does not exist.'}, 404
-        
-        data =api.payload
-        if not data:
-            return {'message':'No input data detected'}, 400
         try:
+            db_salesman = Salesman.fetch_by_id(id)
+            this_salesman = salesman_schema.dump(db_salesman)
+            if len(this_salesman) == 0:
+                return {'message': 'This salesman does not exist.'}, 404
+            
+            data =api.payload
+            if not data:
+                return {'message':'No input data detected'}, 400
+            
             limit = data['limit']
             Salesman.update_salesman(id=id, limit=limit)
             db_salesman = Salesman.fetch_by_id(id)
@@ -168,11 +174,11 @@ class SalesmanOperations(Resource):
             print('========================================')
             print('Error description: ', e)
             print('========================================')
-            return {'message': 'Couldn\'t update salesman'}, 400
+            return {'message': 'Could not update salesman'}, 500
 
 
     @jwt_required
-    @api.doc('delete_salesman')
+    @api.doc('Delete salesman')
     def delete(self, id):
         '''Delete Salesman'''
         authorised_user = get_jwt_identity()
@@ -198,34 +204,40 @@ class SalesmanOperations(Resource):
             print('========================================')
             print('Error description: ', e)
             print('========================================')
-            return {'message': 'Couldn\'t delete salesman'}, 400
+            return {'message': 'Could not delete salesman'}, 500
 
 @api.route('/user/<int:user_id>')
 @api.param('user_id', 'The user identifier')
 class GetSalesmanUser(Resource):
     @jwt_required
-    @api.doc('get_salesman_by_ user')
+    @api.doc('Get salesman by user')
     def get(self, user_id):
         '''Get Salesman by User'''
         authorised_user = get_jwt_identity()
         claims = get_jwt_claims()
 
-        if authorised_user['id'] == user_id or claims['is_admin']:
-            db_salesman = Salesman.fetch_by_user_id(user_id=user_id)
-            salesman = salesman_schema.dump(db_salesman)
+        try:
+            if authorised_user['id'] == user_id or claims['is_admin']:
+                db_salesman = Salesman.fetch_by_user_id(user_id=user_id)
+                salesman = salesman_schema.dump(db_salesman)
 
-            if len(salesman) == 0:
-                return {'message': 'There is no such salesman'}, 404
+                if len(salesman) == 0:
+                    return {'message': 'There is no such salesman'}, 404
 
-            # Record this event in user's logs
-            log_method = 'get'
-            log_description = 'Fetched salesman by user id <' + str(user_id) + '>' 
-            authorization = request.headers.get('Authorization')
-            auth_token  = { "Authorization": authorization}
-            record_user_log(auth_token, log_method, log_description)
+                # Record this event in user's logs
+                log_method = 'get'
+                log_description = 'Fetched salesman by user id <' + str(user_id) + '>' 
+                authorization = request.headers.get('Authorization')
+                auth_token  = { "Authorization": authorization}
+                record_user_log(auth_token, log_method, log_description)
 
-            return {'salesman': salesman}, 200
-        return {'message': 'You are not authorised to  view this salesman!'}, 403
+                return {'salesman': salesman}, 200
+            return {'message': 'You are not authorised to  view this salesman!'}, 403
+        except Exception as e:
+            print('========================================')
+            print('Error description: ', e)
+            print('========================================')
+            return{'message':'Could not fetch salesman.'}, 500
 
         
 
@@ -261,7 +273,7 @@ class SuspendSalesman(Resource):
             print('========================================')
             print('Error description: ', e)
             print('========================================')
-            return {'message': 'Couldn\'t delete salesman'}, 400
+            return {'message': 'Could not delete salesman'}, 500
 
 @api.route('/restore/<int:id>')
 @api.param('id', 'The salesman identifier')
@@ -295,4 +307,4 @@ class RestoreSalesman(Resource):
             print('========================================')
             print('Error description: ', e)
             print('========================================')
-            return {'message': 'Couldn\'t delete salesman'}, 400
+            return {'message': 'Could not delete salesman'}, 500
